f5721ed4d0cd23a3f71dc559c0593c6e9251fa29
Madhavan N <152672405+madhavan1402@users.noreply.github.com>
2026-02-20 12:50:48 +0530

    Update app.py

--- ml-api/app.py
+++ ml-api/app.py
@@ -34,5 +34,6 @@ def extract_skills(text):
     return found
 
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    app.run(host=host, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
